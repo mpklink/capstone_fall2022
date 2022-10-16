aeb4257331fa3fd0f988fba2888153f4c210951c
lute47lillo <lutelillocs47@gmail.com>
2022-10-16 12:29:35 -0500

    Transition on Trigger collision

---
+++ Assets/Scripts/OverWorldToTransition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverWorldToTransition : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.name == "FPSController")
+        {
+            Debug.Log("Collision detected, Transition to Lobby");
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+}
